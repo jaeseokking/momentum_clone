bbdcef610d50de729ca8b9b5c660a2197aa75d52
jaeseokking <72733192+jaeseokking@users.noreply.github.com>
2022-01-17 22:23:36 +0900

    Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,29 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const TODOS_KEY = "todos"
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY  , JSON.stringify(toDos));
+}
+
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
-    const span = document = document.createElement("span");
-    li.appendChild(span);
+    const span = document.createElement("span");
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText ="X";
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -14,8 +32,21 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value; //깊은 복사
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 
 }
 
+
 toDoForm.addEventListener("submit", handleToDoSubmit)
+
+function sayHello(item){
+    console.log("hello", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(item => {sayHello(item)});
+}
